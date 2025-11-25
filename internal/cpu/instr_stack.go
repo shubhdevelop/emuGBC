1ad450c93580586fae6c623de2bcf25695f14685
shubham <shubhamxraj10@gmail.com>
2025-11-25 22:12:24 +0530

    feat(cpu): implement stack push and pop instructions for registers
    - Added methods for popping values from the stack into registers BC, DE, HL, and AF.
    - Implemented methods for pushing values from registers BC, DE, HL, and AF onto the stack.
    - Each push and pop operation returns the appropriate cycle count for execution.

---
+++ internal/cpu/instr_stack.go
@@ -0,0 +1,39 @@
+package cpu
+
+func (cpu *CPU) InstrSTACK_POP_BC() int {
+	val := cpu.Pop()
+	cpu.Registers.SetBC(val)
+	return 12
+}
+func (cpu *CPU) InstrSTACK_POP_DE() int {
+	val := cpu.Pop()
+	cpu.Registers.SetDE(val)
+	return 12
+}
+func (cpu *CPU) InstrSTACK_POP_HL() int {
+	val := cpu.Pop()
+	cpu.Registers.SetHL(val)
+	return 12
+}
+func (cpu *CPU) InstrSTACK_POP_AF() int {
+	val := cpu.Pop()
+	cpu.Registers.SetAF(val)
+	return 12
+}
+
+func (cpu *CPU) InstrSTACK_PUSH_BC() int {
+	cpu.Push(cpu.Registers.GetBC())
+	return 16
+}
+func (cpu *CPU) InstrSTACK_PUSH_DE() int {
+	cpu.Push(cpu.Registers.GetBC())
+	return 16
+}
+func (cpu *CPU) InstrSTACK_PUSH_HL() int {
+	cpu.Push(cpu.Registers.GetBC())
+	return 16
+}
+func (cpu *CPU) InstrSTACK_PUSH_AF() int {
+	cpu.Push(cpu.Registers.GetBC())
+	return 16
+}
