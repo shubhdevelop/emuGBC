736adb5cd6d368c1f509d6197da1fb430c5c2719
shubham <shubhamxraj10@gmail.com>
2025-11-25 15:08:52 +0530

    feat(cpu): implement jump instructions for absolute and relative addressing
    - Added various jump instructions including JumpAbsolute, JumpAbsoluteNotZero, JumpAbsoluteZero, JumpAbsoluteNotCarry, JumpAbsoluteCarry, JumpAbsoluteHL, JumpRelative, JumpRelativeNotZero, JumpRelativeNotCarry, JumpRelativeZero, and JumpRelativeCarry.
    - Each instruction updates the program counter (PC) based on specific conditions and returns the appropriate cycle count.

--- internal/cpu/instr_jump.go
+++ internal/cpu/instr_jump.go
@@ -1,1 +1,116 @@
 package cpu
+
+func (cpu *CPU) JumpAbsolute() int {
+	target := cpu.Bus.ReadWord(cpu.Registers.PC)
+	cpu.Registers.PC = target
+	return 16
+}
+
+func (cpu *CPU) JumpAbsoluteNotZero() int {
+	target := cpu.Bus.ReadWord(cpu.Registers.PC)
+	if !cpu.Registers.GetFlag(FlagZ) {
+		cpu.Registers.PC = target
+		return 16
+	} else {
+		return 12
+	}
+}
+
+func (cpu *CPU) JumpAbsoluteZero() int {
+	target := cpu.Bus.ReadWord(cpu.Registers.PC)
+	if cpu.Registers.GetFlag(FlagZ) {
+		cpu.Registers.PC = target
+		return 16
+	} else {
+		return 12
+	}
+}
+
+func (cpu *CPU) JumpAbsoluteNotCarry() int {
+	target := cpu.Bus.ReadWord(cpu.Registers.PC)
+	if !cpu.Registers.GetFlag(FlagC) {
+		cpu.Registers.PC = target
+		return 16
+	} else {
+		return 12
+	}
+}
+
+func (cpu *CPU) JumpAbsoluteCarry() int {
+	target := cpu.Bus.ReadWord(cpu.Registers.PC)
+	if cpu.Registers.GetFlag(FlagC) {
+		cpu.Registers.PC = target
+		return 16
+	} else {
+		return 12
+	}
+}
+
+func (cpu *CPU) JumpAbsoluteHL() int {
+	target := cpu.Registers.GetHL()
+	cpu.Registers.PC = target
+	return 4
+}
+
+func (cpu *CPU) JumpRelative() int {
+
+	offset := cpu.Bus.Read(cpu.Registers.PC)
+	cpu.Registers.PC++
+	currentPC := int32(cpu.Registers.PC)
+	jump := int32(int8(offset)) // 0xFE becomes -2
+
+	cpu.Registers.PC = uint16(currentPC + jump)
+	return 12
+}
+
+func (cpu *CPU) JumpRelativeNotZero() int {
+	offset := int8(cpu.Bus.Read(cpu.Registers.PC))
+	cpu.Registers.PC++
+
+	// 2. Check the condition
+	if !cpu.Registers.GetFlag(FlagZ) {
+		cpu.Registers.PC = uint16(int16(offset))
+		return 12
+	} else {
+		return 8
+	}
+}
+
+func (cpu *CPU) JumpRelativeNotCarry() int {
+	offset := int8(cpu.Bus.Read(cpu.Registers.PC))
+	cpu.Registers.PC++
+
+	// 2. Check the condition
+	if !cpu.Registers.GetFlag(FlagC) {
+		cpu.Registers.PC = uint16(int16(offset))
+		return 12
+	} else {
+		return 8
+	}
+}
+
+func (cpu *CPU) JumpRelativeZero() int {
+	offset := int8(cpu.Bus.Read(cpu.Registers.PC))
+	cpu.Registers.PC++
+
+	// 2. Check the condition
+	if cpu.Registers.GetFlag(FlagZ) {
+		cpu.Registers.PC = uint16(int16(offset))
+		return 12
+	} else {
+		return 8
+	}
+}
+
+func (cpu *CPU) JumpRelativeCarry() int {
+	offset := int8(cpu.Bus.Read(cpu.Registers.PC))
+	cpu.Registers.PC++
+
+	// 2. Check the condition
+	if cpu.Registers.GetFlag(FlagC) {
+		cpu.Registers.PC = uint16(int16(offset))
+		return 12
+	} else {
+		return 8
+	}
+}
--- internal/cpu/instr_load.go
+++ internal/cpu/instr_load.go
@@ -5,9 +5,10 @@ OPCODE: 0x01
 DESCRIPTION: Load 16 bits into BC
 CYCLE: 12
 */
-func (cpu *CPU) InstrLD_BC_d16() {
+func (cpu *CPU) InstrLD_BC_d16() int {
 	w := cpu.FetchWord()
 	cpu.Registers.SetBC(w)
+	return 12
 }
 
 /*
@@ -15,9 +16,10 @@ OPCODE: 0x02
 DESCRIPTION: Load A into BC
 CYCLE: 8
 */
-func (cpu *CPU) InstrLD_BC_A() {
+func (cpu *CPU) InstrLD_BC_A() int {
 	addr := cpu.Registers.GetBC()
 	cpu.Bus.Write(addr, cpu.Registers.A)
+	return 8
 }
 
 /*
@@ -25,9 +27,10 @@ OPCODE: 0x06
 DESCRIPTION: Load 8 bits into B
 CYCLE: 8
 */
-func (cpu *CPU) InstrLD_B_d8() {
+func (cpu *CPU) InstrLD_B_d8() int {
 	b := cpu.FetchByte()
 	cpu.Registers.B = b
+	return 8
 }
 
 /*
@@ -35,10 +38,11 @@ OPCODE: 0x08
 DESCRIPTION: store the 16-bit SP value into two bytes of memory at address a16
 CYCLE: 20
 */
-func (cpu *CPU) InstrLD_a16_SP() {
+func (cpu *CPU) InstrLD_a16_SP() int {
 	addr := cpu.FetchWord() // read 16-bit address (little-endian)
 	sp := cpu.Registers.SP  // 16-bit stack pointer
 	cpu.Bus.WriteWord(addr, sp)
+	return 20
 }
 
 /*
@@ -46,10 +50,11 @@ OPCODE: 0x0A
 DESCRIPTION: SET A with [BC]
 CYCLE: 8
 */
-func (cpu *CPU) InstrLD_A_BC() {
+func (cpu *CPU) InstrLD_A_BC() int {
 	addr := cpu.Registers.GetBC() // 16-bit address
 	val := cpu.Bus.Read(addr)     // read 8-bit from memory
 	cpu.Registers.A = val         // load into A
+	return 8
 }
 
 /*
@@ -57,9 +62,10 @@ OPCODE: 0x0E
 DESCRIPTION: SET C with 8 bits
 CYCLE: 8
 */
-func (cpu *CPU) InstrLD_C_d8() {
+func (cpu *CPU) InstrLD_C_d8() int {
 	b := cpu.FetchByte()
 	cpu.Registers.C = b
+	return 8
 }
 
 /*
@@ -67,9 +73,10 @@ OPCODE: 0x11
 DESCRIPTION: SET DE with 16 bits
 CYCLE: 12
 */
-func (cpu *CPU) InstrLD_DE_d16() {
+func (cpu *CPU) InstrLD_DE_d16() int {
 	w := cpu.FetchWord()
 	cpu.Registers.SetDE(w)
+	return 12
 }
 
 /*
@@ -77,10 +84,11 @@ OPCODE: 0x12
 DESCRIPTION: SET [DE] with A
 CYCLE: 8
 */
-func (cpu *CPU) InstrLD_DE_A() {
+func (cpu *CPU) InstrLD_DE_A() int {
 	addr := cpu.Registers.GetDE()
 	val := cpu.Registers.A
 	cpu.Bus.Write(addr, val)
+	return 8
 }
 
 /*
@@ -88,9 +96,10 @@ OPCODE: 0x16
 DESCRIPTION: SET D with 8 bits
 CYCLE: 8
 */
-func (cpu *CPU) InstrLD_D_d8() {
+func (cpu *CPU) InstrLD_D_d8() int {
 	b := cpu.FetchByte()
 	cpu.Registers.D = b
+	return 8
 }
 
 /*
@@ -98,10 +107,11 @@ OPCODE: 0x1A
 DESCRIPTION: SET A with [DE]
 CYCLE: 8
 */
-func (cpu *CPU) InstrLD_A_DE() {
+func (cpu *CPU) InstrLD_A_DE() int {
 	addr := cpu.Registers.GetDE()
 	b := cpu.Bus.Read(addr)
 	cpu.Registers.A = b
+	return 8
 }
 
 /*
@@ -109,7 +119,8 @@ OPCODE: 0x1E
 DESCRIPTION: SET E with 8 bits
 CYCLE: 8
 */
-func (cpu *CPU) InstrLD_E_d8() {
+func (cpu *CPU) InstrLD_E_d8() int {
 	b := cpu.FetchByte()
 	cpu.Registers.E = b
+	return 8
 }
