708038e10bcd801a64e881dc04d304ffda29918a
shubham <shubhamxraj10@gmail.com>
2025-11-25 22:12:10 +0530

    feat(cpu): implement RET and CALL instructions for stack control
    - Added RET, RETI, and conditional RET instructions to manage return operations based on flags.
    - Implemented CALL and conditional CALL instructions to facilitate function calls with stack management.
    - Introduced RST instructions for direct jumps to specific memory addresses, enhancing control flow capabilities.

--- internal/cpu/instr_jump.go
+++ internal/cpu/instr_jump.go
@@ -114,3 +114,141 @@ func (cpu *CPU) JumpRelativeCarry() int {
 		return 8
 	}
 }
+
+func (cpu *CPU) RET() int {
+	cpu.Registers.PC = cpu.Pop()
+	return 16
+}
+
+func (cpu *CPU) RETI() int {
+	cpu.Registers.PC = cpu.Pop()
+	cpu.IME = true
+	return 16
+}
+
+func (cpu *CPU) RET_NZ() int {
+	if !cpu.Registers.GetFlag(FlagZ) {
+		cpu.Registers.PC = cpu.Pop()
+		return 20
+	} else {
+		return 8
+	}
+}
+func (cpu *CPU) RET_NC() int {
+	if !cpu.Registers.GetFlag(FlagC) {
+		cpu.Registers.PC = cpu.Pop()
+		return 20
+	} else {
+		return 8
+	}
+}
+
+func (cpu *CPU) RET_Z() int {
+	if cpu.Registers.GetFlag(FlagZ) {
+		cpu.Registers.PC = cpu.Pop()
+		return 20
+	} else {
+		return 8
+	}
+}
+
+func (cpu *CPU) RET_C() int {
+	if cpu.Registers.GetFlag(FlagC) {
+		cpu.Registers.PC = cpu.Pop()
+		return 20
+	} else {
+		return 8
+	}
+}
+
+func (cpu *CPU) CALL() int {
+	w := cpu.FetchWord()
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = w
+	return 24
+}
+
+func (cpu *CPU) CALL_C() int {
+	w := cpu.FetchWord()
+	if cpu.Registers.GetFlag(FlagC) {
+		cpu.Push(cpu.Registers.PC)
+		cpu.Registers.PC = w
+		return 24
+	} else {
+		return 12
+	}
+}
+
+func (cpu *CPU) CALL_Z() int {
+	w := cpu.FetchWord()
+	if cpu.Registers.GetFlag(FlagZ) {
+		cpu.Push(cpu.Registers.PC)
+		cpu.Registers.PC = w
+		return 24
+	} else {
+		return 12
+	}
+}
+
+func (cpu *CPU) CALL_NC() int {
+	w := cpu.FetchWord()
+	if !cpu.Registers.GetFlag(FlagC) {
+		cpu.Push(cpu.Registers.PC)
+		cpu.Registers.PC = w
+		return 24
+	} else {
+		return 12
+	}
+}
+
+func (cpu *CPU) CALL_NZ() int {
+	w := cpu.FetchWord()
+	if !cpu.Registers.GetFlag(FlagZ) {
+		cpu.Push(cpu.Registers.PC)
+		cpu.Registers.PC = w
+		return 24
+	} else {
+		return 12
+	}
+}
+
+func (cpu *CPU) RST_00H() int {
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = 0x0000
+	return 16
+}
+func (cpu *CPU) RST_10H() int {
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = 0x0010
+	return 16
+}
+func (cpu *CPU) RST_20H() int {
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = 0x0020
+	return 16
+}
+func (cpu *CPU) RST_30H() int {
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = 0x0030
+	return 16
+}
+func (cpu *CPU) RST_08H() int {
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = 0x0008
+	return 16
+}
+func (cpu *CPU) RST_18H() int {
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = 0x0018
+	return 16
+}
+func (cpu *CPU) RST_28H() int {
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = 0x0028
+	return 16
+}
+func (cpu *CPU) RST_38H() int {
+	cpu.Push(cpu.Registers.PC)
+	cpu.Registers.PC = 0x0038
+	return 16
+}
