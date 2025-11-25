90d28376fcbf7c242889b6cbd59414451d0ae2b7
shubham <shubhamxraj10@gmail.com>
2025-11-25 18:31:24 +0530

    feat(cpu): implement CONT instructions for CPU control
    - Added CONT_DI, CONT_EI, CONT_HALT, and CONT_STOP instructions to manage CPU state.
    - Updated IME and isHalted flags accordingly during instruction execution.
    - Implemented logic for the CONT_STOP instruction to handle automatic opcode consumption.

--- internal/cpu/instr_control.go
+++ internal/cpu/instr_control.go
@@ -1,1 +1,28 @@
 package cpu
+
+func (cpu *CPU) InstrCONT_DI() int {
+	cpu.IME = false
+	return 4
+}
+
+func (cpu *CPU) InstrCONT_EI() int {
+	cpu.IME = true
+	return 4
+}
+
+func (cpu *CPU) InstrCONT_HALT() int {
+	cpu.isHalted = true
+	return 4
+}
+
+/*
+When the CPU sees the opcode 0x10,
+it is hardwired to automatically consume the next byte
+(which is almost always 0x00) before going to sleep.
+*/
+func (cpu *CPU) InstrCONT_STOP() int {
+	//It's part of the instruction!
+	cpu.Registers.PC++
+	cpu.isHalted = true
+	return 4
+}
--- internal/cpu/instr_logic.go
+++ internal/cpu/instr_logic.go
@@ -1,1 +1,10 @@
 package cpu
+
+func (cpu *CPU) InstructLOGIC_XOR_A() int {
+	cpu.Registers.A ^= cpu.Registers.A
+	cpu.Registers.SetFlag(FlagZ, cpu.Registers.A == 0)
+	cpu.Registers.SetFlag(FlagN, false)
+	cpu.Registers.SetFlag(FlagH, false)
+	cpu.Registers.SetFlag(FlagC, false)
+	return 4
+}
